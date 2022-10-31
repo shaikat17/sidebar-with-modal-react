e795d1b01ffb2ec8b452346c7131fc4ffa597c93
shaikat17 <shaikatpal56@gmail.com>
2022-10-31 12:53:26 +0600

    Working with the context api

---
+++ src/context.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+}
+
+// Custom hook
+
+export { AppContext, AppProvider }
